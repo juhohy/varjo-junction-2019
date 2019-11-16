f8113ff8fa424bac83a18a4b0eaa2635344cc2c6
Petteri <->
2019-11-16 14:54:05 +0200

    Add InkCard script

---
+++ Junction2019/Assets/Scripts/InkCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkCard : MonoBehaviour
+{
+    public float gazeSeconds { get; private set; }
+    private void Update()
+    {
+        if (GazeManager.instance.gazeHitTarget?.GetComponentInParent<InkCard>() == this)
+        {
+            gazeSeconds += Time.deltaTime;
+        }
+    }
+}
