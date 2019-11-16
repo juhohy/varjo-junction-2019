eb33e90bd3bf24c0d65edac5f52164c3a14a29b6
Samuli <samuli.jaaskelainen@varjo.com>
2019-11-16 18:52:47 +0200

    Implement graphics to gorilla balls.

---
+++ Junction2019/Assets/Scripts/Bee.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up + transform.right, 240.0f * Time.deltaTime, Space.World);
+    }
+}
