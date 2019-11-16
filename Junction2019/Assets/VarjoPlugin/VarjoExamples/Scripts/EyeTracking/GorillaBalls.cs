de53c5f58090f0e7a00ebdb0fe5c236993630f30
Samuli <samuli.jaaskelainen@varjo.com>
2019-11-16 11:02:56 +0200

    Gorilla ball logic & prefabify scenarios.

--- Junction2019/Assets/VarjoPlugin/VarjoExamples/Scripts/EyeTracking/GorillaBalls.cs
+++ Junction2019/Assets/VarjoPlugin/VarjoExamples/Scripts/EyeTracking/GorillaBalls.cs
@@ -7,14 +7,26 @@ public class GorillaBalls : MonoBehaviour
     public TalkAndDo description;
     public TalkAndDo endcription;
 
+    public Transform ballPos1;
+    public Transform ballPos2;
+    public Transform ball;
+    public AnimationCurve ballCurve;
+    public float ballCurveMultiplier = 1.0f;
+    int currentThrow;
+    int throwAmount;
+
+
     void OnEnable()
     {
+        currentThrow = 0;
+        ball.gameObject.SetActive(false);
+        ball.position = ballPos1.position;
         description.Perfrom();
     }
 
     void RunTest()
     {
-        Invoke("EndTest", 10.0f);
+        StartCoroutine("TestRoutine");
     }
 
     void EndTest()
@@ -22,4 +34,28 @@ public class GorillaBalls : MonoBehaviour
         endcription.Perfrom();
     }
 
+    IEnumerator TestRoutine()
+    {
+        bool dir1to2 = true;
+        while (currentThrow < throwAmount)
+        {
+            yield return new  WaitForSeconds(1.0f);
+
+            float throwVal = 0.0f;
+            while(throwVal < 1.0f)
+            {
+                ball.position = dir1to2 ? Vector3.Lerp(ballPos1.position, ballPos2.position, throwVal) : Vector3.Lerp(ballPos2.position, ballPos1.position, throwVal);
+                float height = ballCurve.Evaluate(throwVal) * ballCurveMultiplier;
+                ball.position += Vector3.up * height;
+
+                throwVal += Time.deltaTime;
+                yield return null;
+            }
+
+            throwAmount++;
+            dir1to2 = !dir1to2;
+            yield return null;
+        }
+
+    }
 }
