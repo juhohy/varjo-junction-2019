a15a1966fd178042b759d68f5246c00b75206645
Samuli <samuli.jaaskelainen@varjo.com>
2019-11-16 17:07:28 +0200

    Moose.

---
+++ Junction2019/Assets/Scripts/Eye.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(Varjo.VarjoManager.Instance.HeadTransform);
+    }
+}
--- Junction2019/Assets/Scripts/Moose.cs
+++ Junction2019/Assets/Scripts/Moose.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Moose : MonoBehaviour
 {
+    public Animator animator;
+    public Collider col;
 
+    private void Update()
+    {
+        bool lookingAt = GazeManager.instance.gazeHitTarget == col;
+        float target = animator.GetFloat("Blend");
+        target = Mathf.Lerp(target, lookingAt ? 1.0f : 0.0f, Time.deltaTime * 10.0f);
+        animator.SetFloat("Blend", target);
+    }
 }
