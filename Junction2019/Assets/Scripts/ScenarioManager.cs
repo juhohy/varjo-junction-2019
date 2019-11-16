f6c3c3dfa4264fbae3abec8691afab71db448cc7
Samuli <samuli.jaaskelainen@varjo.com>
2019-11-16 19:56:51 +0200

    Add music player. Make sound effects use positional audio.

--- Junction2019/Assets/Scripts/ScenarioManager.cs
+++ Junction2019/Assets/Scripts/ScenarioManager.cs
@@ -6,9 +6,13 @@ public class ScenarioManager : MonoBehaviour
 {
     int currentScenario = 0;
     public List<GameObject> scenarios = new List<GameObject>();
+    public List<AudioClip> scenarioMusics = new List<AudioClip>();
+    AudioSource musicSource;
 
     void Awake()
     {
+        musicSource = gameObject.AddComponent<AudioSource>();
+        musicSource.loop = true;
         LoadCurrentScenario();
     }
 
@@ -32,6 +36,13 @@ public class ScenarioManager : MonoBehaviour
         for (int i = 0; i < scenarios.Count; ++i)
         {
             scenarios[i].SetActive(i == currentScenario);
+        }
+
+        if(scenarioMusics.Count > currentScenario)
+        {
+            Debug.Log("Playing scenario music: " + scenarioMusics[currentScenario].name);
+            musicSource.clip = scenarioMusics[currentScenario];
+            musicSource.Play();
         }
     }
 
